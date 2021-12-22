0994b87d01107b1b281f2101b2937ac7eed64218
danielmihaiflorea <andidaniflorea@gmail.com>
2021-12-22 23:35:54 +0200

    commit

---
+++ src/Iarasi.java
@@ -0,0 +1,15 @@
+
+public class Iarasi {
+
+	public static void main(String[] args) {
+	int indent = 1;
+	String unu = "hai";
+	String doi = "ca";
+	String trei = "este";
+	String patru = "a";
+	String cinci = "doua";
+	System.out.println(indent + " " + unu + doi + trei + patru + cinci + "," + "bravo" + "!");
+
+	}
+
+}
